e7969a429db10bd345221ba161bcae6c12067139
jasonzyx <17521797+jasonzyx@users.noreply.github.com>
2022-01-10 10:40:14 -0800

    comments list

--- twitter/settings.py
+++ twitter/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # rest frameworks
     'rest_framework',
+    'django_filters',
     # project apps
     'accounts',
     'debug_toolbar',
@@ -49,7 +50,10 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
 }
 
 MIDDLEWARE = [
@@ -132,3 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+try:
+    from .local_settings import *
+except:
+    pass
