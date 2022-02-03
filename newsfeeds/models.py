a27382e12d2fcddc1b42471222b29bc33aeb725d
jasonzyx <17521797+jasonzyx@users.noreply.github.com>
2022-02-03 10:22:05 -0800

    tweet cache

--- newsfeeds/models.py
+++ newsfeeds/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tweets.models import Tweet
+from utils.memcached_helper import MemcachedHelper
 
 
 class NewsFeed(models.Model):
@@ -23,4 +24,9 @@ class NewsFeed(models.Model):
 
     def __str__(self):
         return f'{self.created_at} tweet shown to {self.user}: {self.tweet}'
+
+    @property
+    def cached_tweet(self):
+        return MemcachedHelper.get_object_through_cache(Tweet, self.tweet_id)
+
 
