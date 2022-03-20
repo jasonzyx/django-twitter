0820812c0cf4ccaab19ac457485f30d21f6c8bd5
jasonzyx <17521797+jasonzyx@users.noreply.github.com>
2022-03-20 14:48:31 -0700

    django hbase initial commit

--- twitter/settings.py
+++ twitter/settings.py
@@ -108,6 +108,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
+HBASE_HOST = '127.0.0.1'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
